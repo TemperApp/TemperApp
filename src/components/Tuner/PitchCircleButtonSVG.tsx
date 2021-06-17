ebb2e58b693e47e5f9b8059e4ce9d5b5d54a5f02
benjamin <benjamin27briere@orange.fr>
2021-06-17 11:05:12 +0200

    [Tuner] Click Gestion Begin

--- src/components/Tuner/PitchCircleButtonSVG.tsx
+++ src/components/Tuner/PitchCircleButtonSVG.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
+
+type DivOrNullType = SVGPathElement | null;
 
 type PitchCircleButtonSVGProps = {
   position: string,
+  active: boolean
 }
 
-const PitchCircleButtonSVG: React.FC<PitchCircleButtonSVGProps> = ({position}) => {
+const PitchCircleButtonSVG: React.FC<PitchCircleButtonSVGProps> = ({position, active}) => {
+
+  const note = useRef<DivOrNullType>(null);
+
+  const handleClick = () => {
+    console.log("click");
+  }
+
+  
   return (
-    <path fill="#F5FBFB" stroke="#A7C5C3" strokeOpacity=".5" strokeWidth="2" d={position}/>
+    <path fill={(active)?"#A7C5C3":"#F5FBFB"} stroke="#A7C5C3" strokeOpacity=".5" strokeWidth="2" d={position} ref={note} onClick={handleClick}/>
   );
 };
 
