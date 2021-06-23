22effa4440eb0bd8c1f61408df49b7bb7b482609
ludchieng <lud.chieng@hotmail.fr>
2021-06-23 11:26:08 +0200

    [sqlite]: Add temp proxy for dev only

--- src/engine/DataAccessor.tsx
+++ src/engine/DataAccessor.tsx
@@ -2,11 +2,23 @@
 import { Temperament } from '../model/Temperament';
 import DB, { DivergenceDBType, TemperamentDBType } from '../engine/DB';
 
+// TODO Remove on production
+import { defaultTemperaments } from '../model/Temperament';
+
+
 /**
  * @returns All known temperaments in database
  */
 export const fetchTemperaments = async (): Promise<Array<TemperamentDBType>> => {
+  // TODO Remove on production
+  return defaultTemperaments.map((t) => ({
+    idTemperament: t.idTemperament,
+    name: t.name,
+    nameFR: t.nameFR,
+  }));
+  /*
   return await DB.query("SELECT * FROM temperament");
+  */
 };
 
 
@@ -17,7 +29,18 @@ export const fetchTemperaments = async (): Promise<Array<TemperamentDBType>> => 
 export const fetchTemperamentById = async (
   id: number
 ): Promise<TemperamentDBType> => {
+  // TODO Remove on production
+  return defaultTemperaments
+    .filter((t) => t.idTemperament === id)
+    .map((t) => ({
+      idTemperament: t.idTemperament,
+      name: t.name,
+      nameFR: t.nameFR,
+    }))[0];
+  // TODO Handle id not found
+  /*
   return (await DB.query("SELECT * FROM temperament WHERE idTemperament = ?;", [id]))[0];
+  */
 };
 
 
@@ -29,6 +52,11 @@ export const fetchTemperamentById = async (
 export const fetchTemperamentPropsById = async (
   id: number
 ): Promise<Temperament> => {
+  // TODO Remove on production
+  return defaultTemperaments
+    .filter((t) => t.idTemperament === id)[0];
+  // TODO Handle id not found
+  /*
   const tmpmt = await fetchTemperamentById(id) as TemperamentDBType;
   const divergence = await DB.query(
     "SELECT * FROM divergence WHERE idTemperament = ?", [id]);
@@ -36,6 +64,7 @@ export const fetchTemperamentPropsById = async (
   const cpExp5th  = divergence.reduce((acc, d: DivergenceDBType) => (acc[d.noteSymbol] = d.cpExp5th, acc), {});
   const csExp3rd  = divergence.reduce((acc, d: DivergenceDBType) => (acc[d.noteSymbol] = d.csExp3rd, acc), {});
   return { ...tmpmt, deviation, cpExp5th, csExp3rd };
+  */
 };
 
 
@@ -45,10 +74,14 @@ export const fetchTemperamentPropsById = async (
  */
 export const fetchAllTemperamentsProps = async (
 ): Promise<Array<Temperament>> => {
+  // TODO Remove on production
+  return defaultTemperaments;
+  /*
   const temperaments = await fetchTemperaments();
   const res = Promise.all(temperaments.map(async (tmpmt: TemperamentDBType) => {
     return await fetchTemperamentPropsById(tmpmt.idTemperament);
   }));
   DB.close();
   return await res;
+  */
 };
