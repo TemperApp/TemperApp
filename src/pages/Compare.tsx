f431775510f8e1c432d0bdb0120d13aa886d20dd
benjamin <benjamin27briere@orange.fr>
2021-06-25 09:56:16 +0200

    [All] Update Header

--- src/pages/Compare.tsx
+++ src/pages/Compare.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import './Compare.css';
 import Comparator from '../components/Comparator/Comparator';
 
@@ -7,8 +7,19 @@ const Compare: React.FC = () => {
   return (
     <IonPage>
       <IonHeader className="ion-no-border">
-        <IonToolbar>
-          <IonTitle>Compare</IonTitle>
+      <IonToolbar>
+          {/*<IonTitle>Tune</IonTitle>*/}
+          <IonGrid className="ion-padding-horizontal headerPages">
+            <IonRow className="ion-align-items-center">
+              <IonCol size="8" >
+                <h1>COMPARATOR</h1>
+              </IonCol>
+              <IonCol size="1" offset="2">
+                <IonButton></IonButton>
+              </IonCol>
+            </IonRow>
+            <div className="gradientBar"></div>
+          </IonGrid>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen scrollY={true}>
--- src/pages/Tune.tsx
+++ src/pages/Tune.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
 import Tuner from '../components/Tuner/Tuner';
 import './Tune.css';
+import { TemperamentDBType } from '../engine/DB';
 
 const Tune: React.FC = () => {
   return (
     <IonPage>
       <IonHeader className="ion-no-border">
         <IonToolbar>
-          <IonTitle>Tune</IonTitle>
+          {/*<IonTitle>Tune</IonTitle>*/}
+          <IonGrid className="ion-padding-horizontal">
+            <IonRow className="ion-align-items-center">
+              <IonCol size="5" >
+                <h1>TUNE</h1>
+              </IonCol>
+              <IonCol size="1" offset="5">
+                <IonButton></IonButton>
+              </IonCol>
+            </IonRow>
+            <div className="gradientBar"></div>
+          </IonGrid>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen scrollY={false}>
