6143f61c2d11d2511948b43fda103a7cc8013cbc
benjamin <benjamin27briere@orange.fr>
2021-06-25 10:09:26 +0200

    [All] Update Header

--- src/pages/Compare.tsx
+++ src/pages/Compare.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import './Compare.css';
 import Comparator from '../components/Comparator/Comparator';
+import { playCircle } from 'ionicons/icons';
 
 const Compare: React.FC = () => {
   return (
@@ -9,18 +10,20 @@ const Compare: React.FC = () => {
       <IonHeader className="ion-no-border">
       <IonToolbar>
           {/*<IonTitle>Tune</IonTitle>*/}
-          <IonGrid className="ion-padding-horizontal headerPages">
-            <IonRow className="ion-align-items-center">
-              <IonCol size="8" >
-                <h1>COMPARATOR</h1>
-              </IonCol>
-              <IonCol size="1" offset="2">
-                <IonButton></IonButton>
-              </IonCol>
-            </IonRow>
-            <div className="gradientBar"></div>
-          </IonGrid>
-        </IonToolbar>
+        <IonGrid className="ion-padding-horizontal headerPages">
+          <IonRow className="ion-align-items-center">
+            <IonCol size="8" >
+              <h1>COMPARATOR</h1>
+            </IonCol>
+            <IonCol size="1" offset="2">
+              <IonButton className="btn-header-modal">
+                <IonIcon slot="icon-only" icon={playCircle} />
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonToolbar>
+      <div className="gradientBar"></div>
       </IonHeader>
       <IonContent fullscreen scrollY={true}>
         <Comparator /> 
