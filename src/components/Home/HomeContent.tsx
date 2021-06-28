067122a70e089a84df20ab36827db3daa640d6c1
Sterenn <sterenn.fonseca@gmail.com>
2021-06-28 09:54:17 +0200

    [Home] Update - Logo - SVG

--- src/components/Home/HomeContent.tsx
+++ src/components/Home/HomeContent.tsx
@@ -1,12 +1,13 @@
+/* eslint-disable eqeqeq */
 import "rc-collapse/assets/index.css";
 import Collapse, { Panel } from "rc-collapse";
 import motion from './_util/motionUtil';
-import { IonButton, IonGrid, IonRow, IonCol, IonIcon } from '@ionic/react';
-import { chevronDownOutline, chevronForwardSharp } from "ionicons/icons";
+import { IonButton, IonGrid, IonRow, IonCol } from '@ionic/react';
 import { useState } from "react";
 
 import './Collapse.css'; 
 import "./ButtonTemper.css"
+import Waves from "./Waves";
 
 const arrowPath =
 'M869 487.8L491.2 159.9c-2.9-2.5-6.6-3.9-10.5-3.9h-88' +
@@ -14,8 +15,13 @@ const arrowPath =
 '6 8 8 8h585.1L386.9 854c-5.6 4.9-2.2 14 5.2 14h91.5c1.9 0 3.8-0.7 5.' +
 '2-2L869 536.2c14.7-12.8 14.7-35.6 0-48.4z';
 
+
+
 const HomeContent : React.FC = () => {
     const temperList = ["Rameau", "Vallotti", "Weimeister", "Back", "Egal"];
+
+    const [firstUse, setFirstUse] = useState<boolean>(true); 
+    let activePanel = 1; 
 
     const [statePanel1, setStatePanel1] = useState<boolean>(false);
     const [statePanel2, setStatePanel2] = useState<boolean>(false);
@@ -38,7 +44,44 @@ const HomeContent : React.FC = () => {
     }
 
     const expandIcon = (e : any) => {
-        //console.log(e.panelKey);
+        console.log(e.panelKey);
+        console.log(firstUse);
+
+        
+        if(firstUse){
+            if(activePanel==0){
+                setStatePanel1(true); 
+                setFirstUse(false); 
+            }
+            if(activePanel==1){
+                setStatePanel2(true); 
+                setFirstUse(false); 
+            }
+        }
+            /*
+            if(e.panelKey == activePanel){
+                return (
+                    <i style={{ marginRight: '.5rem' }}>
+                    <svg
+                        viewBox="0 0 1024 1024"
+                        width="1em"
+                        height="1em"
+                        fill="currentColor"
+                        style={{
+                        verticalAlign: '-.125em',
+                        transition: 'transform 0.2s',
+                        transform: `rotate(${statePanel1 ? 90 : 0}deg)`,
+                        }}
+                    >
+                        <path d={arrowPath} p-id="5827" />
+                    </svg>
+                    </i>
+                )
+            }
+        }
+            */
+
+
         if(e.panelKey == 0){
             return (
                 <i style={{ marginRight: '.5rem' }}>
@@ -83,29 +126,32 @@ const HomeContent : React.FC = () => {
     return (
     <div>
    
-    <Collapse accordion={true} openMotion={motion} expandIcon={(e) => expandIcon(e)} onChange={(e) => (isActive(e))} >
-        <Panel header="Mes tempéraments" headerClass="my-header-class" >
+    <Collapse accordion={true} defaultActiveKey={activePanel} openMotion={motion} expandIcon={(e) => expandIcon(e)} onChange={(e) => (isActive(e))} >
+        <Panel key="0" header="Mes tempéraments" headerClass="my-header-class" >
             <IonGrid>
                 <IonRow >
                     {temperList.map((t) => 
                     <IonCol size='6'>
-                        <IonButton expand="block" color="temperapp">{t}</IonButton>
+                        <IonButton className="buttonType" expand="block" color="temperapp">{t}</IonButton>
                     </IonCol>
                      )}
                 </IonRow>
             </IonGrid>
         </Panel>
-        <Panel header="Les plus courants" headerClass="my-header-class"> <IonGrid>
+        <Panel key="1" header="Les plus courants" headerClass="my-header-class"> <IonGrid>
                 <IonRow >
                     {temperList.map((t) => 
                     <IonCol size='6'>
-                        <IonButton expand="block" color="temperapp">{t}</IonButton>
+                        <IonButton className="buttonType" expand="block" color="temperapp">{t}</IonButton>
                     </IonCol>
                      )}
                 </IonRow>
             </IonGrid>
         </Panel>
     </Collapse>
+
+    <Waves />
+    
 
     </div>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCol, IonContent, IonGrid, IonHeader, IonImg, IonPage, IonRow, IonToolbar, IonIcon } from '@ionic/react';
 import './Home.css';
 import HomeContent from '../components/Home/HomeContent';
+import {ellipsisVertical} from "ionicons/icons";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader className="ion-no-border">
         <IonToolbar>
-          <IonTitle>TemperApp</IonTitle>
+          <IonGrid>
+                <IonRow>
+                    <IonCol size='2' className="Col1">
+                    </IonCol>
+                    <IonCol className ="Col2" size='8'>
+                       <IonImg src={"../../assets/icon/Logo_TemperApp.png"} />
+                    </IonCol>
+                    <IonCol size='2' className="Col3">
+                      <IonIcon icon={ellipsisVertical} size="large"></IonIcon>
+                    </IonCol>
+                </IonRow>
+            </IonGrid>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen scrollY={false}>
