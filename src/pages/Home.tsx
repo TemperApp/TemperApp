113cbe69a95228a79b70a2e8f661fbadbd72988d
benjamin <benjamin27briere@orange.fr>
2021-06-28 10:58:34 +0200

    [Home] Dark Theme Toggle

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToggle, IonToolbar } from '@ionic/react';
 import './Home.css';
 import HomeContent from '../components/Home/HomeContent';
 
 const Home: React.FC = () => {
+
+  const change = (e : any) => {
+    console.log(e.target.checked);
+    document.body.classList.toggle('dark', e.target.checked);
+  }
+
   return (
     <IonPage>
       <IonHeader className="ion-no-border">
         <IonToolbar>
           <IonTitle>TemperApp</IonTitle>
         </IonToolbar>
+        <IonList>
+        <IonItem lines="full">
+          <IonIcon slot="start" name="moon"></IonIcon>
+          <IonLabel>
+            Je suis les ombres
+          </IonLabel>
+          <IonToggle  id="themeToggle" slot="end" onClick={e => change(e)}></IonToggle>
+        </IonItem>
+      </IonList>
       </IonHeader>
       <IonContent fullscreen scrollY={false}>
         <HomeContent />
