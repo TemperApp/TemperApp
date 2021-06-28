8be9539b10672b7a9b90438b598a9ff88cd3c064
Daphné C-R <60186128+TSU50R1@users.noreply.github.com>
2021-06-28 15:07:15 +0200

    [style] starting the homogeinisation

--- src/components/Header/HeaderPage.tsx
+++ src/components/Header/HeaderPage.tsx
@@ -1,36 +1,50 @@
-import { IonCol, IonContent, IonGrid, IonHeader, IonModal, IonPage, IonRow, IonToolbar } from '@ionic/react';
-import React, { useState } from 'react';
+import {
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonModal,
+  IonPage,
+  IonRow,
+  IonToolbar,
+} from "@ionic/react";
+import React, { useState } from "react";
 
 type headerProps = {
-  buttonModal: boolean,
-  buttonModalText: string,
-  setShowModal: (state: boolean) => void, 
-}
+  buttonModal: boolean;
+  buttonModalText: string;
+  setShowModal: (state: boolean) => void;
+};
 
-const HeaderPage: React.FC<headerProps> = ({buttonModal,buttonModalText, setShowModal}) => {
-
+const HeaderPage: React.FC<headerProps> = ({
+  buttonModal,
+  buttonModalText,
+  setShowModal,
+}) => {
   let title = "COMPARATOR";
 
-  if(buttonModal){
-    return(
-      <IonHeader className="ion-no-border">
+  if (buttonModal) {
+    return (
+      <IonHeader>
         <IonToolbar>
           <IonGrid className="ion-padding-horizontal headerPages">
             <IonRow className="ion-align-items-center">
-              <IonCol size="8" >
+              <IonCol size="8">
                 <h1>{buttonModalText}</h1>
               </IonCol>
-              <IonCol size='1' offset='2'>
-                <div className = 'btn-header-modal' onClick={() => setShowModal(true)}>
+              <IonCol size="1" offset="2">
+                <div
+                  className="btn-header-modal"
+                  onClick={() => setShowModal(true)}
+                >
                   <span>?</span>
                 </div>
               </IonCol>
             </IonRow>
           </IonGrid>
         </IonToolbar>
-        <div className="gradientBar"></div>
       </IonHeader>
-    )
+    );
   }
 
   return (
@@ -39,7 +53,7 @@ const HeaderPage: React.FC<headerProps> = ({buttonModal,buttonModalText, setShow
         <IonToolbar>
           <IonGrid className="ion-padding-horizontal headerPages">
             <IonRow className="ion-align-items-center">
-              <IonCol size="8" >
+              <IonCol size="8">
                 <h1>{title}</h1>
               </IonCol>
             </IonRow>
