cc07291c848a0895e4f9624041f265c6f4535178
Daphné C-R <60186128+TSU50R1@users.noreply.github.com>
2021-06-30 13:42:24 +0200

    [style] fixed comparator wheel in dark theme

--- src/components/Comparator/ComparatorNotesCircle.tsx
+++ src/components/Comparator/ComparatorNotesCircle.tsx
@@ -1,29 +1,98 @@
-import React from 'react';
+import React from "react";
 
 //Style
-import './Comparator.css'
+import "./Comparator.css";
 
 const ComparatorNotesCircle: React.FC = () => {
-
   return (
     <>
       <g id="ComparatorNotes">
-        <path className="st21" d="M200,43.76c-86.29,0-156.24,69.95-156.24,156.24S113.71,356.24,200,356.24S356.24,286.29,356.24,200
+        <path
+          className="st21"
+          d="M200,43.76c-86.29,0-156.24,69.95-156.24,156.24S113.71,356.24,200,356.24S356.24,286.29,356.24,200
           S286.29,43.76,200,43.76z M197.96,273.21c-41.55,0-75.24-33.69-75.24-75.25c0-41.55,33.69-75.24,75.24-75.24
-          c41.56,0,75.25,33.69,75.25,75.24C273.21,239.52,239.52,273.21,197.96,273.21z"/>
-        <g id="Texte">
-          <text transform="matrix(1 0 0 1 311.39 273.2095)" className="st22 st23">E</text>
-          <text transform="matrix(1 0 0 1 262.7041 325.1587)" className="st22 st23">B</text>
-          <text transform="matrix(1 0 0 1 190.2009 341.2029)"><tspan x="0" y="0" className="st22 st23">F</tspan><tspan x="10.93" y="0" className="st24 st23">♯</tspan></text>
-          <text transform="matrix(1 0 0 1 122.5 325.1595)"><tspan x="0" y="0" className="st22 st23">C</tspan><tspan x="12.67" y="0" className="st24 st23">♯</tspan></text>
-          <text transform="matrix(1 0 0 1 68.3599 273.2103)"><tspan x="0" y="0" className="st22 st23">G</tspan><tspan x="13.54" y="0" className="st24 st23">♯</tspan></text>
-          <text transform="matrix(1 0 0 1 52.7483 204.4795)"><tspan x="0" y="0" className="st22 st23">E</tspan><tspan x="11.73" y="0" className="st25 st23">♭</tspan></text>
-          <text transform="matrix(1 0 0 1 71.6626 135.3268)"><tspan x="0" y="0" className="st22 st23">B</tspan><tspan x="12.96" y="0" className="st25 st23">♭</tspan></text>
-          <text transform="matrix(1 0 0 1 126.365 88.354)" className="st22 st23">F</text>
-          <text transform="matrix(1 0 0 1 193.9 69.1483)" className="st22 st23">C</text>
-          <text transform="matrix(1 0 0 1 264.3253 88.5921)" className="st22 st23">G</text>
-          <text transform="matrix(1 0 0 1 311.3904 134.8376)" className="st22 st23">D</text>
-          <text transform="matrix(1 0 0 1 328.3633 206.1)" className="st22 st23">A</text>
+          c41.56,0,75.25,33.69,75.25,75.24C273.21,239.52,239.52,273.21,197.96,273.21z"
+        />
+        <g id="Texte" fill="var(--color-contrast)">
+          <text
+            transform="matrix(1 0 0 1 311.39 273.2095)"
+            className="st22 st23"
+          >
+            E
+          </text>
+          <text
+            transform="matrix(1 0 0 1 262.7041 325.1587)"
+            className="st22 st23"
+          >
+            B
+          </text>
+          <text transform="matrix(1 0 0 1 190.2009 341.2029)">
+            <tspan x="0" y="0" className="st22 st23">
+              F
+            </tspan>
+            <tspan x="10.93" y="0" className="st24 st23">
+              ♯
+            </tspan>
+          </text>
+          <text transform="matrix(1 0 0 1 122.5 325.1595)">
+            <tspan x="0" y="0" className="st22 st23">
+              C
+            </tspan>
+            <tspan x="12.67" y="0" className="st24 st23">
+              ♯
+            </tspan>
+          </text>
+          <text transform="matrix(1 0 0 1 68.3599 273.2103)">
+            <tspan x="0" y="0" className="st22 st23">
+              G
+            </tspan>
+            <tspan x="13.54" y="0" className="st24 st23">
+              ♯
+            </tspan>
+          </text>
+          <text transform="matrix(1 0 0 1 52.7483 204.4795)">
+            <tspan x="0" y="0" className="st22 st23">
+              E
+            </tspan>
+            <tspan x="11.73" y="0" className="st25 st23">
+              ♭
+            </tspan>
+          </text>
+          <text transform="matrix(1 0 0 1 71.6626 135.3268)">
+            <tspan x="0" y="0" className="st22 st23">
+              B
+            </tspan>
+            <tspan x="12.96" y="0" className="st25 st23">
+              ♭
+            </tspan>
+          </text>
+          <text
+            transform="matrix(1 0 0 1 126.365 88.354)"
+            className="st22 st23"
+          >
+            F
+          </text>
+          <text transform="matrix(1 0 0 1 193.9 69.1483)" className="st22 st23">
+            C
+          </text>
+          <text
+            transform="matrix(1 0 0 1 264.3253 88.5921)"
+            className="st22 st23"
+          >
+            G
+          </text>
+          <text
+            transform="matrix(1 0 0 1 311.3904 134.8376)"
+            className="st22 st23"
+          >
+            D
+          </text>
+          <text
+            transform="matrix(1 0 0 1 328.3633 206.1)"
+            className="st22 st23"
+          >
+            A
+          </text>
         </g>
       </g>
     </>
