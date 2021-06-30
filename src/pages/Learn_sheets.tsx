9454a9e15f7342bae5f0b61ee48732ff7db667f7
Sterenn <sterenn.fonseca@gmail.com>
2021-06-30 14:53:13 +0200

    [Learn] Chapiter Mise en page

--- src/pages/Learn_sheets.tsx
+++ src/pages/Learn_sheets.tsx
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import {
   IonButton,
+  IonCol,
   IonContent,
+  IonGrid,
   IonHeader,
   IonItem,
   IonLabel,
   IonList,
   IonPage,
+  IonRouterLink,
   IonRouterOutlet,
+  IonRow,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
@@ -37,6 +41,8 @@ const Learn: React.FC<LearnProps> = ({darkTheme}) => {
 };
 */
 
+
+
 const Learn_sheets: React.FC<RouteComponentProps> = ({ match, history }) => {
   const [showModal, setShowModal] = useState(false);
 
@@ -52,21 +58,89 @@ const Learn_sheets: React.FC<RouteComponentProps> = ({ match, history }) => {
         darkTheme={false}
       />
 
-      <IonContent fullscreen scrollY={true}>
-        <IonList>
-          <IonItem routerLink="/learn/sheet/1">
-            <IonLabel>Definition</IonLabel>
-          </IonItem>
-          <IonItem routerLink="/learn/sheet/2">
-            <IonLabel>Physique</IonLabel>
-          </IonItem>
-          <IonItem routerLink="/learn/sheet/3">
-            <IonLabel>Caractéristiques</IonLabel>
-          </IonItem>
-          <IonItem routerLink="/learn/sheet/4">
-            <IonLabel>Histoire</IonLabel>
-          </IonItem>
-        </IonList>
+      <IonContent fullscreen scrollY={true}> 
+        <IonGrid>
+          <IonRouterLink routerLink="/learn/sheet/1">
+            <IonRow className="ion-padding-top">
+              <IonCol size='4' className="ColPictoLearn">
+                <svg width="71" height="71" viewBox="0 0 71 71" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="35.5" cy="35.5" r="35.5" fill="url(#paint0_linear)"/>
+                  <defs>
+                  <linearGradient id="paint0_linear" x1="59.6844" y1="35.5" x2="-25.5062" y2="31.9786" gradientUnits="userSpaceOnUse">
+                  <stop stop-color="#FFE0B1"/>
+                  <stop offset="0.480556" stop-color="#FFC09F"/>
+                  <stop offset="1" stop-color="#F88F8F"/>
+                  </linearGradient>
+                  </defs>
+                </svg>
+              </IonCol>
+              <IonCol size ="8" className="ColChapiterLearn">
+                <h3>Definition</h3>
+              </IonCol>
+            </IonRow>
+          </IonRouterLink>
+
+
+          <IonRouterLink routerLink="/learn/sheet/2">
+            <IonRow className="ion-padding-top">
+              <IonCol size='4' className="ColPictoLearn">
+                <svg width="71" height="71" viewBox="0 0 71 71" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="35.5" cy="35.5" r="35.5" fill="url(#paint0_linear)"/>
+                  <defs>
+                  <linearGradient id="paint0_linear" x1="59.6844" y1="35.5" x2="-25.5062" y2="31.9786" gradientUnits="userSpaceOnUse">
+                  <stop stop-color="#FFE0B1"/>
+                  <stop offset="0.480556" stop-color="#FFC09F"/>
+                  <stop offset="1" stop-color="#F88F8F"/>
+                  </linearGradient>
+                  </defs>
+                </svg>
+              </IonCol>
+              <IonCol size ="8" className="ColChapiterLearn">
+                <h3>Caractéristiques</h3>
+              </IonCol>
+            </IonRow>
+          </IonRouterLink>
+
+          <IonRouterLink routerLink="/learn/sheet/3">
+            <IonRow className="ion-padding-top">
+              <IonCol size='4' className="ColPictoLearn">
+                <svg width="71" height="71" viewBox="0 0 71 71" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="35.5" cy="35.5" r="35.5" fill="url(#paint0_linear)"/>
+                  <defs>
+                  <linearGradient id="paint0_linear" x1="59.6844" y1="35.5" x2="-25.5062" y2="31.9786" gradientUnits="userSpaceOnUse">
+                  <stop stop-color="#FFE0B1"/>
+                  <stop offset="0.480556" stop-color="#FFC09F"/>
+                  <stop offset="1" stop-color="#F88F8F"/>
+                  </linearGradient>
+                  </defs>
+                </svg>
+              </IonCol>
+              <IonCol size ="8" className="ColChapiterLearn">
+                <h3>Physique</h3>
+              </IonCol>
+            </IonRow>
+          </IonRouterLink>
+
+          <IonRouterLink routerLink="/learn/sheet/4">
+            <IonRow className="ion-padding-top">
+              <IonCol size='4' className="ColPictoLearn">
+                <svg width="71" height="71" viewBox="0 0 71 71" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="35.5" cy="35.5" r="35.5" fill="url(#paint0_linear)"/>
+                  <defs>
+                  <linearGradient id="paint0_linear" x1="59.6844" y1="35.5" x2="-25.5062" y2="31.9786" gradientUnits="userSpaceOnUse">
+                  <stop stop-color="#FFE0B1"/>
+                  <stop offset="0.480556" stop-color="#FFC09F"/>
+                  <stop offset="1" stop-color="#F88F8F"/>
+                  </linearGradient>
+                  </defs>
+                </svg>
+              </IonCol>
+              <IonCol size ="8" className="ColChapiterLearn">
+                <h3>Histoire</h3>
+              </IonCol>
+            </IonRow>
+          </IonRouterLink>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
