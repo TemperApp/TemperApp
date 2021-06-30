e3efdce7df79afa7ce49b7b04982908d0ce5f686
Daphné C-R <60186128+TSU50R1@users.noreply.github.com>
2021-06-30 14:58:31 +0200

    [style] home logo in dark mode

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,51 @@
-import React from 'react';
-import { IonCol, IonContent, IonGrid, IonHeader, IonImg, IonPage, IonRow, IonToolbar } from '@ionic/react';
-import './Home.css';
-import HomeContent from '../components/Home/HomeContent';
-import ParameterModal from '../components/Home/ParameterModal'; 
+import React from "react";
+import {
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonImg,
+  IonPage,
+  IonRow,
+  IonToolbar,
+} from "@ionic/react";
+import "./Home.css";
+import HomeContent from "../components/Home/HomeContent";
+import ParameterModal from "../components/Home/ParameterModal";
+import { isDarkTheme } from "../model/Utils";
 
 type HomeProps = {
-  darkTheme : boolean,
-  setDarkTheme: (color: boolean) => void,
-}
+  darkTheme: boolean;
+  setDarkTheme: (color: boolean) => void;
+};
 
-const Home: React.FC<HomeProps> = ({darkTheme,setDarkTheme}) => {
-  const change = (e : any) => {
+const Home: React.FC<HomeProps> = ({ darkTheme, setDarkTheme }) => {
+  const change = (e: any) => {
     console.log(e.target.checked);
-    document.body.classList.toggle('dark', e.target.checked);
-    (e.target.checked === true)?setDarkTheme(true):setDarkTheme(false);
-  }
+    document.body.classList.toggle("dark", e.target.checked);
+    e.target.checked === true ? setDarkTheme(true) : setDarkTheme(false);
+  };
   return (
     <IonPage>
       <IonHeader className="ion-no-border">
         <IonToolbar>
           <IonGrid>
             <IonRow>
-              <IonCol size='2' className="ColTemperApp">
+              <IonCol size="2" className="ColTemperApp"></IonCol>
+              <IonCol className="ColTemperApp " size="8">
+                <IonImg
+                  src={
+                    isDarkTheme()
+                      ? "../../assets/logotypes/temperapp_logo-dark.svg"
+                      : "../../assets/logotypes/temperapp_logo.svg"
+                  }
+                  className="logo-home"
+                />
               </IonCol>
-              <IonCol className ="ColTemperApp " size='8'>
-                <IonImg src={"../../assets/logotypes/temperapp_logo.svg"} />
-              </IonCol>
-              <IonCol size='2' className="ColTemperApp">
-                <ParameterModal 
-                  darkTheme = {darkTheme}
-                  setDarkTheme = {setDarkTheme}
+              <IonCol size="2" className="ColTemperApp">
+                <ParameterModal
+                  darkTheme={darkTheme}
+                  setDarkTheme={setDarkTheme}
                 />
               </IonCol>
             </IonRow>
@@ -37,9 +53,7 @@ const Home: React.FC<HomeProps> = ({darkTheme,setDarkTheme}) => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen scrollY={false}>
-        <HomeContent
-          darkTheme = {darkTheme}
-        />
+        <HomeContent darkTheme={darkTheme} />
       </IonContent>
     </IonPage>
   );
