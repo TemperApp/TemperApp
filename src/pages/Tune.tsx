c1912dcc822bf7dfd3080f6e37726af36e398d88
Daphné C-R <60186128+TSU50R1@users.noreply.github.com>
2021-06-30 11:33:23 +0200

    [all] fixed merge conflicts

--- src/pages/Tune.tsx
+++ src/pages/Tune.tsx
@@ -1,33 +1,10 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
-import {
-  IonButton,
-  IonCol,
-  IonContent,
-  IonGrid,
-  IonHeader,
-  IonModal,
-  IonPage,
-  IonRow,
-  IonSelect,
-  IonSelectOption,
-  IonTitle,
-  IonToolbar,
-} from "@ionic/react";
+import { IonContent, IonPage } from "@ionic/react";
 import Tuner from "../components/Tuner/Tuner";
 import "./Tune.css";
 import { TemperamentDBType } from "../engine/DB";
 import HeaderPage from "../components/Header/HeaderPage";
 import TunerModal from "../components/Tuner/TunerModal";
-=======
-import React, { useState } from 'react';
-import {IonContent, IonPage} from '@ionic/react';
-import Tuner from '../components/Tuner/Tuner';
-import './Tune.css';
-import { TemperamentDBType } from '../engine/DB';
-import HeaderPage from '../components/Header/HeaderPage';
-import TunerModal from '../components/Tuner/TunerModal';
->>>>>>> e718b091309b07016d041acd585dd7a6fad1ca5d
 
 type TuneProps = {
   darkTheme: boolean;
@@ -39,22 +16,14 @@ const Tune: React.FC<TuneProps> = ({ darkTheme }) => {
 
   return (
     <IonPage>
-<<<<<<< HEAD
       <HeaderPage
         doubleTitle={true}
         buttonModal={true}
         buttonModalsubText={`${isHzMode ? "Battements" : "Pitch pipe"}`}
+        buttonReturn={false}
         buttonModalText="Accordeur"
         setShowModal={setShowModal}
         darkTheme={darkTheme}
-=======
-      <HeaderPage 
-        buttonModal = {true}
-        buttonReturn={false}
-        buttonModalText = "TUNER"
-        setShowModal = {setShowModal}
-        darkTheme = {darkTheme}
->>>>>>> e718b091309b07016d041acd585dd7a6fad1ca5d
       />
       <IonContent fullscreen scrollY={false}>
         <TunerModal
