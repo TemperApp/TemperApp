305ab6625bfb9fe8e6094292db54e71067d02781
Daphné C-R <60186128+TSU50R1@users.noreply.github.com>
2021-07-07 15:31:48 +0200

    [style] updated tuner-help

--- src/pages/Tune.tsx
+++ src/pages/Tune.tsx
@@ -18,7 +18,7 @@ import Tuner from "../components/Tuner/Tuner";
 
 const TuneModalContent: React.FC = () => (
   <>
-    <h4>Qualité des quintes et des tierces</h4>
+    <h4 className="py-3">Qualité des quintes et des tierces</h4>
     <p className="p-long">
       Le visuel TemperApp vous permet de prévoir en un coup
       d’oeil la sonorité de tous les accords majeurs dans
@@ -31,17 +31,17 @@ const TuneModalContent: React.FC = () => (
       tierces sont représentées à l’extérieur du cycle, les
       quintes à l’intérieur.
     </p>
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center py-5">
       <div
         className="h-12 w-12 mr-4 rounded-full"
         style={{ border: "2px solid var(--color-contrast)" }} />
       <div
-        className="mr-8">Tierce</div>
+        className="mr-8"><h5>Tierce</h5></div>
       <div
         className="h-6 w-6 mr-4 rounded-full"
         style={{ border: "2px solid var(--color-contrast)" }} />
       <div
-        className="">Quinte</div>
+        className="mr-8"><h5>Quinte</h5></div>
     </div>
     <p className="p-long">
       Plus la couleur de l’intervalle est sombre, plus celui-ci est tempéré. 
@@ -50,21 +50,31 @@ const TuneModalContent: React.FC = () => (
       exceptionnels (quintes élargies, tierces plus grandes que pythagorienne) 
       sont colorés en nuances de gris. 
     </p>
-    <IonCard>
-      <IonCardContent>
-        <IonGrid className="px-4">
+    <IonCard className="flex justify-center items-center w-full py-5">
+      <IonCardContent className="w-full">
+        <IonGrid>
           <IonRow className="justify-end items-end">
             <IonCol size="5" offset="2">
               <strong>Quintes</strong>
+              <div className="h-12 w-12 mr-4"
+                style={{ border: "2px solid var(--color-contrast)",
+                        height: "70vh",
+                        background: "linear-gradient(180deg, #000000 0%, #161616 6.98%, #333333 12.45%, #7E7E7E 16.74%, #9F9F9F 20.27%, #B9B9B9 24.81%, #60CFC5 28.6%, #FFE5BB 31.88%, #FFD592 36.92%, #FFBB75 41.97%, #FF9960 48.53%, #FF764A 53.07%, #FF5435 58.62%, #FF321F 64.68%, #FF0D08 70.23%, #DC0000 77.04%, #E00000 77.81%, #AE0000 83.1%, #7B0000 88.15%, #4F0000 93.45%, #161616 100%)"}}>
+              </div>
             </IonCol>
             <IonCol size="5">
               <strong>Tierces</strong>
+              <div className="h-12 w-12 mr-4"
+                style={{ border: "2px solid var(--color-contrast)",
+                        height: "70vh",
+                        background: "linear-gradient(180deg, #000000 0%, #161616 6.98%, #333333 12.45%, #7E7E7E 16.74%, #9F9F9F 20.27%, #B9B9B9 24.81%, #60CFC5 28.6%, #FFE5BB 31.88%, #FFD592 36.92%, #FFBB75 41.97%, #FF9960 48.53%, #FF764A 53.07%, #FF5435 58.62%, #FF321F 64.68%, #FF0D08 70.23%, #DC0000 77.04%, #E00000 77.81%, #AE0000 83.1%, #7B0000 88.15%, #4F0000 93.45%, #161616 100%)"}}>
+              </div>
             </IonCol>
           </IonRow>
         </IonGrid>
       </IonCardContent>
     </IonCard>
-    <h4>Pitch pipe & battements</h4>
+    <h4 className="py-3">Pitch pipe & battements</h4>
     <strong>TemperApp propose deux accordeurs.</strong>
     <IonGrid>
       <IonRow>
@@ -92,7 +102,7 @@ const TuneModalContent: React.FC = () => (
         </IonCol>
       </IonRow>
     </IonGrid>
-    <h4>Procédure d'accord</h4>
+    <h4 className="py-3">Procédure d'accord</h4>
     <IonGrid>
       <IonRow>
         <IonCol size="3">
