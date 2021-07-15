c126caa1f94a7be956bbc5f68b5350eb081d269d
Briere <benjamin27briere@orange.fr>
2021-07-15 11:23:22 +0200

    [WIP] json file for learn sheets

---
+++ src/model/Learn/learnSheets.tsx
@@ -0,0 +1,20 @@
+
+export type LearnSheet = {
+  id: string,
+};
+
+export const learnSheets: Array<LearnSheet> = [
+  {
+    id: "definition"
+  },
+  {
+    id: "characteristics"
+  },
+  {
+    id: "physics"
+  },
+  {
+    id: "history"
+  },
+
+];
