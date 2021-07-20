2dd1a03209fb37a0d3cd18af160bf42aa77df86b
benjamin <benjamin27briere@orange.fr>
2021-07-20 17:45:59 +0200

    [User] WIP

--- src/store/user-context.tsx
+++ src/store/user-context.tsx
@@ -1,102 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import { useStorageSQLite } from 'react-data-storage-sqlite-hook/dist';
 
-const user = [{
-    name: 'favorite',
-    defaultValue: Array,
-  },
-];
-
-
 const UserContext = React.createContext({
-  favorite: [""],
-  setFavorite: (v: Array<String>) => {},
+  favorite: [''],
+  setFavorite: (v: string[]) => {},
 });
 
-
 export const UserContextProvider: React.FC = ({ children }) => {
-  const {openStore, isStoreOpen, isKey, getItem, setItem, clear, isAvailable} = useStorageSQLite();
-  const store = {openStore, isStoreOpen, isKey, getItem, setItem, clear, isAvailable};
-  
-  /**
-   * @param name setting name
-   * @param defaultValue default value for the setting. It may
-   *                     be overriden by the value in the store
-   *                     or the user input
-   * @param callback1 function to call after updating the state
-   * @returns an object with 3 props. Their name depend on the
-   * 'name' parameter.
-   * (e.g.: 'favorite' gives { favorite, setFavorite, syncFavorite }).
-   * 'favorite' is the getter, 'setFavorite' is the setter, and
-   * 'syncFavorite' will fetch and set the state in the store
-   */
-  const useSetting = <T,>(
-    name: string,
-    defaultValue: T,
-    callback1 = (state: T) => {},
-  ): any => {
-    const [_state, _setState] = useState(defaultValue);
-    
-    // Execute 'setState hook', update the store value and callbacks
-    const setState = (
-      state: T,
-      callback2 = (state: T) => {}
-    ) => {
-      store.setItem(name, String(state));
-      _setState(state);
-      callback1(state);
-      callback2(state);
-    };
+  const {openStore, isKey, getItem, setItem, clear, isAvailable} = useStorageSQLite();
+  const store = {openStore, isKey, getItem, setItem, clear, isAvailable};
 
-    // Synchronize setting state with the value in the store
-    const syncState = () => {
-      (async () => {
-        if (await store.isKey(name))
-          setState(JSON.parse((await store.getItem(name))!));
-      })();
-    }
+  const [favorite, _setFavorite] = useState<string[]>([]);
 
-    return {
-      name: name,
-      get: _state,
-      set: setState,
-      sync: syncState,
-    };
+  const setFavorite = (value: string[]) => {
+    store.setItem('favorite', JSON.stringify(value));
+    _setFavorite(value);
   };
-
-  // Settings list
-  const settingsList: any = user.map((curr: any) => ({
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    ...useSetting(curr.name, curr.defaultValue, curr.callback)
-  }));
-
-  // Provider value
-  const ctxValue: any = settingsList.reduce((acc: any, curr: any) => {
-    const nameCapitalized = `${curr.name.charAt(0).toUpperCase()}${curr.name.slice(1)}`;
-    return {
-      ...acc,
-      [curr.name]: curr.get,
-      [`set${nameCapitalized}`]: curr.set,
-      [`sync${nameCapitalized}`]: curr.sync,
-    };
-  }, {});
-
-
-  // Synchronize settings with values in the store
+  
   useEffect(() => {
     if (store.isAvailable) {
       (async () => {
         await store.openStore({});
-        
-        for(const s of settingsList)
-          s.sync();
+  
+        (async () => {
+          if (await store.isKey('favorite'))
+            setFavorite( JSON.parse( (await store.getItem('favorite'))!) as string[]);
+        })();
+    
       })();
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <UserContext.Provider value={ctxValue}>
+    <UserContext.Provider value={{
+      favorite,
+      setFavorite,
+    }}>
       {children}
     </UserContext.Provider>
   );
