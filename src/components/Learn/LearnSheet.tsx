544c1a052b19d838db43ae3e9a7b85cfdaad0c1b
Sterenn <sterenn.fonseca@gmail.com>
2021-07-22 17:28:33 +0200

    [Learnsheets] update css

--- src/components/Learn/LearnSheet.tsx
+++ src/components/Learn/LearnSheet.tsx
@@ -32,7 +32,7 @@ const LearnSheet: React.FC = () => {
       </IonHeader>
 
       <IonContent className="learn">
-        <div className="px-6 py-2">
+        <div className="pt-3 px-6 p-long markdown-content">
 
           <ReactMarkdown
             remarkPlugins={[remarkMath]}
