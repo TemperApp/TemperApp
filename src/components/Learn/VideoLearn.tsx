60584c9f36388aae7c384d8c6372ceed02d6f110
Daphné C-R <60186128+TSU50R1@users.noreply.github.com>
2021-07-26 17:09:35 +0200

    [style] thumbnail update darktheme

--- src/components/Learn/VideoLearn.tsx
+++ src/components/Learn/VideoLearn.tsx
@@ -1,6 +1,7 @@
 
-import React from 'react';
+import React, { useContext } from 'react';
 import "../../pages/Learn.css"
+import SettingsContext from '../../store/settings-context';
 
 type videoProps = {
   titreText: string,
@@ -8,15 +9,19 @@ type videoProps = {
   thumbnail: string,
 }
 
-const VideoLearn: React.FC<videoProps> = ({ titreText, videoLink,thumbnail }) => {
+const VideoLearn: React.FC<videoProps> = ({ titreText, videoLink }) => {
+  const settings = useContext(SettingsContext);
+
   return (
     <div className="learn-box">
       <h4>{titreText}</h4>
       <a href={videoLink}>
-        <img src="/assets/logotypes/thumbnail.svg" 
-             alt="ressource sonore du tempérament " 
-             className="w-full"
-             style={{fill:"var(--color-grey)"}}
+        <img 
+          src={(settings.darkTheme)
+            ? "/assets/logotypes/thumbnail-dark.svg" 
+            : "/assets/logotypes/thumbnail.svg" }
+          alt="ressource sonore du tempérament " 
+          className="w-full"
         /></a>
       {/* 
       <iframe name="soundReference" src={videoLink.replace("/watch?v=","/embed/")} title="Référence Sonnore" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
