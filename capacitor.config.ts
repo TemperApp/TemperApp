5825e3ef59517049a17522740d148e051289a4fa
ludchieng <lud.chieng@hotmail.fr>
2021-07-27 12:26:14 +0200

    [Build] WIP Name change of App

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'fr.ludchieng.temperapp',
-  appName: 'dev',
+  appName: 'Temperapp',
   webDir: 'build',
   bundledWebRuntime: false
 };
