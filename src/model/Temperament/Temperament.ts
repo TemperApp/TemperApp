d8cf15f8a3a87732def285af6dc5204a91d681c7
TemperApp <88499331+TemperApp@users.noreply.github.com>
2023-06-08 12:04:51 +0200

    Update Temperament.ts
    Vallotti graph add last point

--- src/model/Temperament/Temperament.ts
+++ src/model/Temperament/Temperament.ts
@@ -117,6 +117,7 @@ export const defaultTemperaments: Array<TemperamentData> = [
         { label: 'G#', x: 'Pure', y: '9/11' },
         { label: 'Eb', x: 'Pure', y: '7/11' },
         { label: 'Bb', x: 'Pure', y: '5/11' },
+        { label: '', x: '-1/6', y: '3/11' },
       ],
       scaleX: ['-1/5', '-1/6','-1/8, '-1/12', 'Pure', ' '],
       scaleY: ['3/11', '5/11', '7/11', '9/11', '11/11', ' '],
