e74e6e19874ddac4eb6ed2c093b12fd8ee52c605
jerome Bertier <jerome.bertier@gmail.com>
2023-06-15 12:45:02 +0200

    Adding graph to Equal

--- src/model/Temperament/Equal.ts
+++ src/model/Temperament/Equal.ts
@@ -7,6 +7,13 @@ const EqualTemperament = {
   csExp3rd: mapNotesMap('+7/11'),
   csExpMin3rd: mapNotesMap('-8/11'),
   periodNum: 1800,
+  graph: {
+    data: [
+      { label: 'All Keys', x: '-1/12 Cp', y: '7/11' },
+    ],
+    scaleX: ['','-1/6',' ','-1/8','  ','-1/12 Cp','   ','Pure','    '],
+    scaleY: ['','+3/11 Cs', '5/11', '7/11', '9/11', '11/11', ' '],
+  },
 };
 
 export const fifthEqualQ = (): NotesMap<number|null> => (mapNotesMap(-1));
